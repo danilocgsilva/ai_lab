378b93e0c4fbad4a47faa406d42aac2329cf1ac0
Danilo Silva <contact@danilocgsilva.me>
2025-09-10 17:45:58 -0400

    wip: alura imersion for ia

--- alura_ai_agents_imersion/README.md
+++ alura_ai_agents_imersion/README.md
@@ -1,1 +1,5 @@
 # Local environment for AI exercises
+
+## How to get the key?
+
+Read video content between 10 and 20 minutes.
---
+++ alura_ai_agents_imersion/app/run.py
@@ -0,0 +1,20 @@
+# from google.colab import userdata
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+GOOGLE_API_KEY = os.environ.get('GEMINI_KEY')
+
+modelo = "google/gemma-3-12b"
+# modelo = "google/gemma-3-12b-it"
+
+llm = ChatGoogleGenerativeAI(
+    model=modelo,
+    temperature=0.5,
+    api_key=GOOGLE_API_KEY
+)
+
+resposta_teste = llm.invoke("Qual a capital do Brasil?")
+
+print(resposta_teste)
+
+print("Hello world!")
